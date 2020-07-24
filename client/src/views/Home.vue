fd14629db68dfd2ec0d0fa014566caae30127e29
julioj27 <juliojaya27@gmail.com>
2020-07-24 13:35:44 +0700

    add winner decider

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -13,21 +13,21 @@
         <h1>Room Master</h1>
         <p>{{ roomMaster }}</p>
         <div v-if="isStart===true">
-          <button v-on:click="answer('rock')">Rock</button>
-          <button v-on:click="answer('scissor')">Scissor</button>
-          <button v-on:click="answer('paper')">Paper</button>
+          <button @click="answer('rock')">Rock</button>
+          <button @click="answer('scissor')">Scissor</button>
+          <button @click="answer('paper')">Paper</button>
         </div>
       </div>
       <div class="container">
         <h1>Opponent</h1>
         <p>{{ opponent }}</p>
       </div>
-      <div class="container">
-        <h1>isfinish</h1>
-        <p>{{ isFinish }}</p>
+      <div v-if="isStart===false">
+        <button @click="changeStatus(true)">mulai</button>
       </div>
-      <div v-if="opponent">
-        <button v-on:click="changeStatus(true)">mulai</button>
+      <div class="container" v-if="isFinish===true">
+        <h1>Winner</h1>
+        <p>{{ winner }}</p>
       </div>
     </div>
   </div>
@@ -48,11 +48,12 @@ export default {
       roomName: '',
       roomMaster: '',
       opponent: '',
-      isFinish: '',
       errMsg: '',
+      isFinish: '',
       isStart: false,
       opponentAnswer: '',
-      myAnswer: ''
+      myAnswer: '',
+      winner: ''
     }
   },
   methods: {
@@ -96,7 +97,17 @@ export default {
   watch: {
     finalAnswer: function () {
       if (this.myAnswer && this.opponentAnswer) {
+        this.isFinish = true
         console.log(this.myAnswer, this.opponentAnswer)
+        if (this.myAnswer === this.opponentAnswer) {
+          this.winner = 'game is a tie'
+        } else if (this.myAnswer === 'rock') {
+          this.winner = this.opponentAnswer === 'paper' ? this.opponent : this.roomMaster
+        } else if (this.myAnswer === 'scissor') {
+          this.winner = this.opponentAnswer === 'rock' ? this.opponent : this.roomMaster
+        } else if (this.myAnswer === 'paper') {
+          this.winner = this.opponentAnswer === 'scissor' ? this.opponent : this.roomMaster
+        }
         setTimeout(() => {
           this.myAnswer = ''
           this.opponentAnswer = ''
